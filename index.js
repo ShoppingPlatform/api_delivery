13113fd25645970b999f4e579543b31fe5b946d2
YohanNayanajith <yohannayanajith40@gmail.com>
2022-09-17 00:17:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const orderRoute = require("./routes/order");
+const cors = require("cors");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successful!"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/v1/orders", orderRoute);
+
+// app.listen(process.env.PORT || 5000, () => {
+//   console.log("Backend server is running!");
+// });
+
+app.listen(5002, () => {
+  console.log("Backend delivery server is running!");
+});
